43aac0508ffaeff9f95367c10e026ef8f0556647
SENPAI-BOT-1 <80009152+SENPAI-BOT-1@users.noreply.github.com>
2021-03-04 23:39:16 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,117 @@
+const help = (prefix) => {
+	return `*♞⸺⸺⸺⳹۩᪥۩⳼⸺⸺⸺♞
+	│       *Lista de comandos*
+	│
+	│ 𝐁𝐎𝐓   : *♘ ＢＯＴ P R I N C E ♘*
+	│
+	│ 𝐃𝐎𝐍𝐎    : *♘ S E N P A I ♘*
+	│
+	*♞⸺⸺⸺⳹۩᪥۩⳼⸺⸺⸺♞*/n
+♞ *♘Commandos de figurinha♘* ♞\n
+♘.1.*${prefix}sticker* ou *${prefix}stiker*♘
+♘converte imagem / gif / vídeo em figurinha♘
+♘Como usar : Responder a imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda♘\n
+.♘2.*${prefix}sticker nobg* or *${prefix}stiker nobg*♘
+♘converter imagem em adesivo removendo o fundo♘
+♘Como usar : responder imagem ou enviar imagem com legenda♘\n
+♘.3.*${prefix}toimg*♘
+♘converter adesivo em imagem♘
+♘Como usar : Responder figurinha♘\n
+♘.4.*${prefix}tsticker* ou *${prefix}tstiker*♘
+♘converter texto em adesivo♘
+♘Como usar : *${prefix}tsticker (texto aqui)*♘\n
+♞ *♘Comando de meme♘* ♞\n
+♘.1.*${prefix}meme*
+♘imagens aleatórias de meme [inglês]
+♘Como usar : apenas envie o comando\n
+♞ *♘Outros comandos♘* ♞\n
+♘.1.*${prefix}gtts*
+♘converter texto em fala / áudio
+♘Como usar : *${prefix}gtts [cc] [texto]*♘\n♘Exemplo : *${prefix}gtts ja Oni-chan*♘
+♘nota : use o comando *${prefix}id* para ver tds os idiomas♘\n
+♘.2.*${prefix}loli*♘
+♘imagens aleatórias de loli♘
+♘Como usar : basta enviar o comando♘\n
+♘.3.*${prefix}nsfwloli*♘
+♘imagens aleatórias de nsfw loli♘
+♘Como usar : apenas envie o comando♘\n
+♘.4.*${prefix}url2img*♘
+♘tirar screenshots da web♘
+♘Como usar : *${prefix}url2img [modelo] [url]*♘\n
+♘.5.*${prefix}simi*♘
+♘sua mensagem será respondida por simi♘
+♘Como usar : *${prefix}simi (sua mensagem)*♘\n
+♘.6.*${prefix}ocr*♘
+♘pegue o texto na foto♘
+♘Como usar : responder imagem ou enviar imagem com legenda♘\n
+♘.7.*${prefix}wait*♘
+♘pesquisar anime com imagem [O que anime é isso / aquilo]
+♘Como usar : responder imagem ou enviar imagem com legenda♘\n
+♘.8.*${prefix}setprefix*
+♘substitua o prefixo♘
+♘Como usar : *${prefix}setprefix [texto | opcional]*\n♘Exemplo : *${prefix}setprefix ?*♘
+♘nota: este comando só pode ser usado pelo proprietário do bot♘\n
+♘.9.*${prefix}tiktok*♘
+♘baixa vídeo do tiko tiko♘
+♘Como usar : *${prefix}tiktok (link do vídeo)*♘\n
+♞ *♘Comandos pornograficos♘* ♞\n
+♘.1.*${prefix}hentai1*♘
+♘Foto pornográfica animação 2d♘
+♘.2.*${prefix}hentai2*♘
+♘Foto pornográfica animação 2d♘
+♘.3.*${prefix}cos1*♘
+♘Foto pornográfica de uma conplayer♘
+♘.4.*${prefix}cos2*♘
+♘Foto pornográfica de uma conplayer♘
+♘.5.*${prefix}pika*♘
+♘O nome já diz oq é♘
+♘.6.*${prefix}urlx*♘
+♘Sites pornográficos♘
+♞ *♘Comandos de Grupo♘* ♞\n
+♘.1.*${prefix}add*♘
+♘adicionar membro ao grupo♘
+♘Como uisar : *${prefix}add 556598xxxxxxx*♘\n
+♘nota: só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!♘\n
+♘.2.*${prefix}kick*♘
+♘tirar membro do grupo♘
+♘Como usar : *${prefix}kick @tagmember*♘\n
+♘nota : só pode ser usado quando o bot se torna admin e quem envia o comando é admin!♘\n
+♘.3.*${prefix}promote*♘
+♘tornar o membro do grupo como administrador do grupo♘
+♘Como usar : *${prefix}promote @tagmember*♘\n
+♘nota: só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!♘\n
+♘.4.*${prefix}demote*♘
+♘tornar o administrador do grupo como membro do grupo♘
+♘Como usar : *${prefix}demote @tagmember*♘\n
+♘nota: só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!♘\n
+♘.5.*${prefix}linkgroup*♘
+♘pegue o link do grupo♘
+♘Como usar : apenas envie o comando♘
+nota: só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!♘\n
+♘.6.*${prefix}leave*♘
+♘Faça o bot sair do grupo♘
+♘Como usar : apenas envie o comando♘
+♘nota: só pode ser usado por administradores do grupo e proprietário do bot♘\n
+♘.7.*${prefix}tagall*♘
+♘marca todos os membros do grupo, incluindo administradores também♘
+♘Como usar : apenas envie o comando♘
+♘nota: este comando pode ser usado se você for um administrador de grupo♘\n
+♘.8.*${prefix}simih*
+♘activate simi mode in the group
+♘Como usar : *${prefix}simih 1* para ativar o modo simi *${prefix}simih 0* para desativar o modo simi♘
+♘nota: este comando pode ser usado se você for um administrador de grupo♘\n`
+♘.9.*${prefix}tagallink*♘
+♘a msm coisa q o de cima só muda q vai tá em link♘
+♘Como usar : apenas envie o comando♘♘nota: este comando pode ser usado se você for um administrador de grupo♘\n
+♘.10.*${prefix}listadmins*♘
+♘marca tds os adm (somente os adms)♘
+♘Como usar : apenas envie o comando♘
+♞ *♘Créditos♘* ♞\n
+♘.1.*${prefix}dono*♘
+♘créditos do criador do bot♘\n
+🅢🅔🅝🅟🅐🅘 🅞 🅜🅐🅘🅢 🅟🅘🅚🅐 🅕🅓🅢\n
+𝔅𝔒𝔗 𝔓ℜℑ𝔑ℭ𝔈 \n
+https//:Wa.me/+5565981709268
+}
+
+exports.help = help
